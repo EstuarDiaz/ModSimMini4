4bb83a23ded66a78555c2b62dda2cc8d784b4c4e
Yasmin Chavez <ychavez2014ig@gmail.com>
2019-11-18 16:21:58 -0600

    functions debug

--- sketch.js
+++ sketch.js
@@ -82,6 +82,9 @@ class fuzzyFunction {
             let x = (this.x[i] + this.x[i + 1]) / 2;
             areaX += w * h * x;
         }
+        if (area == 0) {
+            return 0;
+        }
         let centroide = areaX / area;
         for (let i = 0; i < this.length; i++) {
             if (this.x[i] >= centroide) {
@@ -98,15 +101,15 @@ class fuzzyFunction {
         vertex(x1, y1);
         fill(102, 178, 255);
         for (let i = 0; i < this.length; i++) {
-            vertex(x1 + (x2 - x1) * (this.x[i] + this.x[0]) / (this.x[this.length - 1] - this.x[0]), y1 + this.y[i] * (y2 - y1));
+            vertex(x1 + (x2 - x1) * (this.x[i] - this.x[0]) / (this.x[this.length - 1] - this.x[0]), y1 + this.y[i] * (y2 - y1));
         }
         vertex(x2, y1);
         endShape(CLOSE);
         if (this.lastEval >= 0) {
             stroke(255, 0, 0);
-            line(x1 + (x2 - x1) * (this.x[this.lastEval] + this.x[0]) / (this.x[this.length - 1] - this.x[0]), y1 + this.y[this.lastEval] * (y2 - y1), x1 + (x2 - x1) * (this.x[this.lastEval] + this.x[0]) / (this.x[this.length - 1] - this.x[0]), y1);
+            line(x1 + (x2 - x1) * (this.x[this.lastEval] - this.x[0]) / (this.x[this.length - 1] - this.x[0]), y1 + this.y[this.lastEval] * (y2 - y1), x1 + (x2 - x1) * (this.x[this.lastEval] - this.x[0]) / (this.x[this.length - 1] - this.x[0]), y1);
             if (n > 0) {
-                line(x1 + (x2 - x1) * (this.x[this.lastEval] + this.x[0]) / (this.x[this.length - 1] - this.x[0]), y1 + this.y[this.lastEval] * (y2 - y1), x2 + n * graphWidth, y1 + this.y[this.lastEval] * (y2 - y1));
+                line(x1 + (x2 - x1) * (this.x[this.lastEval] - this.x[0]) / (this.x[this.length - 1] - this.x[0]), y1 + this.y[this.lastEval] * (y2 - y1), x2 + n * graphWidth, y1 + this.y[this.lastEval] * (y2 - y1));
             }
             stroke(0);
         }
@@ -114,22 +117,24 @@ class fuzzyFunction {
 }
 
 
-let domainDistance = [0, 25, 50, 100, 125, 150, 175, 200, 225, 250, 275, 300, 325, 350, 375, 400, 425, 450, 475, 500, 525, 550, 575, 600, 625, 650];
-let domainDireccion = [-4.0, -3.0, -2.0, -1.0, 0, 1.0, 2.0, 3.0, 4.0]
+let domainDistance = [0., 64., 128., 192., 256., 320., 384., 448., 512., 576., 640.];
+let domainDireccion = [-3.14159265, -2.51327412, -1.88495559, -1.25663706, -0.62831853,
+    0., 0.62831853, 1.25663706, 1.88495559, 2.51327412, 3.14159265
+];
 let domain = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
 
-let lejos = new fuzzyFunction(domainDistance, [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.125, 0.25, 0.375, 0.5, 0.75, 0.875, 1.0]);
-let cerca = new fuzzyFunction(domainDistance, [1.0, 0.875, 0.75, 0.5, 0.375, 0.25, 0.125, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]);
+let lejos = new fuzzyFunction(domainDistance, [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.1, 0.25, 0.5, 0.75, 1.0]);
+let cerca = new fuzzyFunction(domainDistance, [1.0, 0.75, 0.5, 0.25, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]);
 
-let izquierda = new fuzzyFunction(domainDireccion, [1.0, 0.75, 0.50, 0.25, 0.0, 0.0, 0.0, 0.0, 0.0]);
-let derecha = new fuzzyFunction(domainDireccion, [0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.50, 0.75, 1.0]);
-let centro = new fuzzyFunction(domainDireccion, [0.0, 0.25, 0.50, 0.75, 1.0, 0.75, 0.5, 0.25, 0.0]);
+let izquierda = new fuzzyFunction(domainDireccion, [1.0, 0.75, 0.50, 0.25, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]);
+let derecha = new fuzzyFunction(domainDireccion, [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.1, 0.25, 0.50, 0.75, 1.0]);
+let centro = new fuzzyFunction(domainDireccion, [0.0, 0.1, 0.25, 0.50, 0.75, 1.0, 0.75, 0.5, 0.25, 0.1, 0.0]);
 
-let movermeBastante = new fuzzyFunction(domain, [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 5.0, 10.0, 15.0, 20.0]);
-let movermePoco = new fuzzyFunction(domain, [20.0, 15.0, 10.0, 5.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]);
-let girarDerecha = new fuzzyFunction(domain, [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 1.0, 1.5, 2.0]);
-let girarIzquierda = new fuzzyFunction(domain, [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5, -1.0, -1.5, -2.0]);
-let nogirar = new fuzzyFunction(domain, [1.0, 0.5, 0.25, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]);
+let movermeBastante = new fuzzyFunction(domainDistance, [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.5, 0.75, 1.0]);
+let movermePoco = new fuzzyFunction(domainDistance, [1.0, 0.75, 0.5, 0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]);
+let girarDerecha = new fuzzyFunction(domainDireccion, [0.0, 0.0, 0.0, 0.1, 0.1, 0.1, 0.2, 0.25, 0.5, 0.75, 1.0]);
+let girarIzquierda = new fuzzyFunction(domainDireccion, [1.0, 0.75, 0.5, 0.25, 0.2, 0.1, 0.1, 0.1, 0.0, 0.0, 0.0]);
+let nogirar = new fuzzyFunction(domainDireccion, [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]);
 
 function move() {
     PlayerAngle = atan2(BallPos[1] - PlayerPos[1], BallPos[0] - PlayerPos[0]) - HALF_PI;
@@ -148,10 +153,9 @@ function getDistance() {
 }
 
 function resultmove(distance, angle) {
-    PlayerPos[0] = PlayerPos[0] + distance
-    PlayerPos[1] = PlayerPos[1] + distance
     PlayerAngle = atan2(BallPos[1] - PlayerPos[1], BallPos[0] - PlayerPos[0]) - angle;
-
+    PlayerPos[0] = PlayerPos[0] - distance * Math.cos(PlayerAngle);
+    PlayerPos[1] = PlayerPos[1] - distance * Math.sin(PlayerAngle);
 }
 
 function fuzzyOr(f, g) {
@@ -166,7 +170,7 @@ function fuzzyOperation(obj1, obj2, operation) {
     if (obj1.constructor.name == "fuzzyFunction") {
         let f = JSON.parse(JSON.stringify(obj1));
         let g = JSON.parse(JSON.stringify(obj2));
-        let h = new fuzzyFunction(f.x, f.y);
+        let h = new fuzzyFunction(g.x, g.y);
         for (let i = 0; i < h.length; i++) {
             let val = operation(f.y[i], g.y[i]);
             h.y[i] = val;
@@ -209,14 +213,22 @@ function fuzzyAgregation(F) {
 
 function setup() {
     frameRate(1);
-    createCanvas(2 * fieldWidth, fieldHeight);
+    createCanvas(4 * fieldWidth, 4 * fieldHeight);
     ScorePos[0] = (fieldWidth - ScoreWidth) / 2;
     ScorePos[1] = 0;
+    PlayerPos[0] = 0.1 * (fieldWidth - 2 * marginWidth) + marginWidth;
+    PlayerPos[1] = 0.1 * (fieldHeight - 2 * marginWidth) + marginWidth;
+    BallPos[0] = 0.9 * (fieldWidth - 2 * marginWidth) + marginWidth;
+    BallPos[1] = 0.7 * (fieldHeight - 2 * marginWidth) + marginWidth;
+    /*
     PlayerPos[0] = Math.random() * (fieldWidth - 2 * marginWidth) + marginWidth;
     PlayerPos[1] = Math.random() * (fieldHeight - 2 * marginWidth) + marginWidth;
     BallPos[0] = Math.random() * (fieldWidth - 2 * marginWidth) + marginWidth;
     BallPos[1] = Math.random() * (fieldHeight - 2 * marginWidth) + marginWidth;
-    PlayerAngle = Math.random() * 6;
+    */
+    //PlayerAngle = Math.random() * 7;
+    PlayerAngle = 0;
+
 }
 
 function draw() {
@@ -238,8 +250,8 @@ function draw() {
     triangle(-10, -20, 10, -20, 0, 20);
     pop();
 
-    playerDistance = getDistance()
-    playerDireccion = getAngle()
+    playerDistance = getDistance();
+    playerDireccion = getAngle();
 
     // calculo de lasa clausulas de Horn
     //Si esta lejos y a la izquierda   --> moverme bastante y a la derecha
@@ -263,15 +275,79 @@ function draw() {
 
     //Agregación distance
     let FDis = fuzzyAgregation([horn1A, horn2A, horn3A, horn4A, horn5A, horn6A]);
-    let centroideDis = FDis.defuzzify();
+    let centroideDis = FDis.defuzzify() * 0.5;
 
     //Agregación direccion
     let FDir = fuzzyAgregation([horn1B, horn2B, horn3B, horn4B, horn5B, horn6B]);
     let centroideDir = FDir.defuzzify();
 
-    resultmove(centroideDis, centroideDir);
-
-
+    resultmove(0, centroideDir);
+    console.log(centroideDir);
+    // Dibujar las graficas
+    let k = 0; // eje Y
+    let l = 0; // eje X
+    l = 2;
+    // Horn1B
+    lejos.graph(graphLeft + l * graphWidth, graphTop + (k + 0.8) * graphHeigth, graphLeft + (l + 0.8) * graphWidth, graphTop + k * graphHeigth, 3);
+    l++;
+    izquierda.graph(graphLeft + l * graphWidth, graphTop + (k + 0.8) * graphHeigth, graphLeft + (l + 0.8) * graphWidth, graphTop + k * graphHeigth, 2);
+    l++;
+    girarDerecha.graph(graphLeft + l * graphWidth, graphTop + (k + 0.8) * graphHeigth, graphLeft + (l + 0.8) * graphWidth, graphTop + k * graphHeigth, 1);
+    l++;
+    horn1B.graph(graphLeft + l * graphWidth, graphTop + (k + 0.8) * graphHeigth, graphLeft + (l + 0.8) * graphWidth, graphTop + k * graphHeigth, 1);
+    // Horn2B
+    k = 1;
+    l = 2;
+    cerca.graph(graphLeft + l * graphWidth, graphTop + (k + 0.8) * graphHeigth, graphLeft + (l + 0.8) * graphWidth, graphTop + k * graphHeigth, 3);
+    l++;
+    izquierda.graph(graphLeft + l * graphWidth, graphTop + (k + 0.8) * graphHeigth, graphLeft + (l + 0.8) * graphWidth, graphTop + k * graphHeigth, 2);
+    l++;
+    girarDerecha.graph(graphLeft + l * graphWidth, graphTop + (k + 0.8) * graphHeigth, graphLeft + (l + 0.8) * graphWidth, graphTop + k * graphHeigth, 1);
+    l++;
+    horn2B.graph(graphLeft + l * graphWidth, graphTop + (k + 0.8) * graphHeigth, graphLeft + (l + 0.8) * graphWidth, graphTop + k * graphHeigth, 1);
+    // Horn3B
+    k = 2;
+    l = 2;
+    lejos.graph(graphLeft + l * graphWidth, graphTop + (k + 0.8) * graphHeigth, graphLeft + (l + 0.8) * graphWidth, graphTop + k * graphHeigth, 3);
+    l++;
+    derecha.graph(graphLeft + l * graphWidth, graphTop + (k + 0.8) * graphHeigth, graphLeft + (l + 0.8) * graphWidth, graphTop + k * graphHeigth, 2);
+    l++;
+    girarIzquierda.graph(graphLeft + l * graphWidth, graphTop + (k + 0.8) * graphHeigth, graphLeft + (l + 0.8) * graphWidth, graphTop + k * graphHeigth, 1);
+    l++;
+    horn3B.graph(graphLeft + l * graphWidth, graphTop + (k + 0.8) * graphHeigth, graphLeft + (l + 0.8) * graphWidth, graphTop + k * graphHeigth, 1);
+    // Horn4B
+    k = 3;
+    l = 2;
+    cerca.graph(graphLeft + l * graphWidth, graphTop + (k + 0.8) * graphHeigth, graphLeft + (l + 0.8) * graphWidth, graphTop + k * graphHeigth, 3);
+    l++;
+    derecha.graph(graphLeft + l * graphWidth, graphTop + (k + 0.8) * graphHeigth, graphLeft + (l + 0.8) * graphWidth, graphTop + k * graphHeigth, 2);
+    l++;
+    girarIzquierda.graph(graphLeft + l * graphWidth, graphTop + (k + 0.8) * graphHeigth, graphLeft + (l + 0.8) * graphWidth, graphTop + k * graphHeigth, 1);
+    l++;
+    horn4B.graph(graphLeft + l * graphWidth, graphTop + (k + 0.8) * graphHeigth, graphLeft + (l + 0.8) * graphWidth, graphTop + k * graphHeigth, 1);
+    // Horn5B
+    k = 4;
+    l = 2;
+    lejos.graph(graphLeft + l * graphWidth, graphTop + (k + 0.8) * graphHeigth, graphLeft + (l + 0.8) * graphWidth, graphTop + k * graphHeigth, 3);
+    l++;
+    centro.graph(graphLeft + l * graphWidth, graphTop + (k + 0.8) * graphHeigth, graphLeft + (l + 0.8) * graphWidth, graphTop + k * graphHeigth, 2);
+    l++;
+    nogirar.graph(graphLeft + l * graphWidth, graphTop + (k + 0.8) * graphHeigth, graphLeft + (l + 0.8) * graphWidth, graphTop + k * graphHeigth, 1);
+    l++;
+    horn5B.graph(graphLeft + l * graphWidth, graphTop + (k + 0.8) * graphHeigth, graphLeft + (l + 0.8) * graphWidth, graphTop + k * graphHeigth, 1);
+    // Horn6B
+    k = 5;
+    l = 2;
+    cerca.graph(graphLeft + l * graphWidth, graphTop + (k + 0.8) * graphHeigth, graphLeft + (l + 0.8) * graphWidth, graphTop + k * graphHeigth, 3);
+    l++;
+    centro.graph(graphLeft + l * graphWidth, graphTop + (k + 0.8) * graphHeigth, graphLeft + (l + 0.8) * graphWidth, graphTop + k * graphHeigth, 2);
+    l++;
+    nogirar.graph(graphLeft + l * graphWidth, graphTop + (k + 0.8) * graphHeigth, graphLeft + (l + 0.8) * graphWidth, graphTop + k * graphHeigth, 1);
+    l++;
+    horn6B.graph(graphLeft + l * graphWidth, graphTop + (k + 0.8) * graphHeigth, graphLeft + (l + 0.8) * graphWidth, graphTop + k * graphHeigth, 1);
+    // Resultado
+    k++;
+    FDir.graph(graphLeft + l * graphWidth, graphTop + (k + 0.8) * graphHeigth, graphLeft + (l + 0.8) * graphWidth, graphTop + k * graphHeigth);
     /*
         // Obtener las graficas de las implicaciones y de agregacion
         let F1 = fuzzyImplication(fuzzyOr(f1.eval(frameCount % 10), f2.eval(frameCount * 2 % 10)), g1);
